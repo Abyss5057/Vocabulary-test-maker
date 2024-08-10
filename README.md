f0c57d5f4a21d8047d5b42db4388f7911a1e60a3
Abyss5057 <417keikunv2@gmail.com>
2024-08-10 14:20:20 +0900

    path fix

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Vocabulary-test-maker
+こっからinstallerおとして実行
+https://github.com/UB-Mannheim/tesseract/wiki
+Additional languageの中のjapaneseにチェック。インスコ先は変えない。
--- test/main.py
+++ test/main.py
@@ -8,6 +8,10 @@ TESSDATA_PATH = TESSERACT_PATH + '\\tessdata'
 os.environ["PATH"] += os.pathsep + TESSERACT_PATH
 os.environ["TESSDATA_PREFIX"] = TESSDATA_PATH
 
+def relativePath(path):
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    return str(cwd + "/" + path)
+
 #OCRエンジン取得
 tools = pyocr.get_available_tools()
 tool = tools[0]
@@ -16,7 +20,7 @@ tool = tools[0]
 builder = pyocr.builders.TextBuilder(tesseract_layout=6)
 
 #解析画像読み込み(雨ニモマケズ)
-img = Image.open('C:\\Users\\Shiina\\Documents\\Repository\\Vocabulary-test-maker\\test\\test.jpg') #他の拡張子でもOK
+img = Image.open(relativePath('test.jpg')) #他の拡張子でもOK
 
 #適当に画像処理(何もしないと結構制度悪いです・・)
 img_g = img.convert('L') #Gray変換
